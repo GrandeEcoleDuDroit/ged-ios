0f5cec794076bd99e4733b4760c9d212a20d57ec
Mourchidi <mourchidi.mfoumby@universite-paris-saclay.fr>
2025-11-14 20:35:33 +0100

    use environment variable to download configuration files

--- ci_scripts/debug_script.sh
+++ ci_scripts/debug_script.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 echo "Download configuration files"
-echo "ORACLE_BUCKET_URL is $ORACLE_BUCKET_URL"
-echo "CI_PROJECT_FILE_PATH is $CI_PROJECT_FILE_PATH"
-
-#curl "$ORACLE_BUCKET_URL"/GoogleService-Info.plist -o $(PROJECT_DIR)/GoogleService-Info.plist
-#curl "$ORACLE_BUCKET_URL"/Config.xconfig -o $(PROJECT_DIR)/Config.xconfig
+curl "$ORACLE_BUCKET_URL"/GoogleService-Info.plist -o $CI_PROJECT_FILE_PATH/GoogleService-Info.plist
+curl "$ORACLE_BUCKET_URL"/Config.xconfig -o $CI_PROJECT_FILE_PATH/Config.xconfig
