510e747120055ad7a3d0f0de1ace18f03ea70a24
Mourchidi <mourchidi.mfoumby@universite-paris-saclay.fr>
2025-11-14 20:55:58 +0100

    fix condition for ci_pre_xcodebuild

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ $CI_PULL_REQUEST_TARGET_BRANCH = "development" && $CI_XCODEBUILD_ACTION = "build" ]; then
+if [ "$CI_PULL_REQUEST_TARGET_BRANCH" = "development" ] && [ "$CI_XCODEBUILD_ACTION" = "build" ]; then
     ./debug_script.sh
 else
-    echo "Not a development branch, skipping debug script"
+    echo "Skipping debug script"
     exit 0
 fi
