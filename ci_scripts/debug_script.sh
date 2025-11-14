89fdfac15e74fa6c1f87419ddb9e2f53c404fe6d
Mourchidi <mourchidi.mfoumby@universite-paris-saclay.fr>
2025-11-14 20:29:14 +0100

    replace PROJECT_DIR with CI_PROJECT_FILE_PATH

--- ci_scripts/debug_script.sh
+++ ci_scripts/debug_script.sh
@@ -2,7 +2,7 @@
 
 echo "Download configuration files"
 echo "ORACLE_BUCKET_URL is $ORACLE_BUCKET_URL"
-echo "PROJECT_DIR is $PROJECT_DIR"
+echo "CI_PROJECT_FILE_PATH is $CI_PROJECT_FILE_PATH"
 
 #curl "$ORACLE_BUCKET_URL"/GoogleService-Info.plist -o $(PROJECT_DIR)/GoogleService-Info.plist
 #curl "$ORACLE_BUCKET_URL"/Config.xconfig -o $(PROJECT_DIR)/Config.xconfig
