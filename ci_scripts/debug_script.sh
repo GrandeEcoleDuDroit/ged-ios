4455d290726ba52bc15478c9135ac6b964e959a5
Mourchidi <mourchidi.mfoumby@universite-paris-saclay.fr>
2025-11-16 02:01:53 +0100

    feat:IGED-137: Add pre build script to download config files, move pre build debug script to debug folder

--- ci_scripts/debug_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Download configuration files"
-curl "$ORACLE_BUCKET_URL"/GoogleService-Info.plist -o $CI_PROJECT_FILE_PATH/GoogleService-Info.plist
-curl "$ORACLE_BUCKET_URL"/Config.xconfig -o $CI_PROJECT_FILE_PATH/Config.xconfig
