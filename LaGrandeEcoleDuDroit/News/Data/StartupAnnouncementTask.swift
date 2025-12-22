81fb82c4820a1e03c93f1a0f4add1b596fc02aeb
Mourchidi <mourchidi.mfoumby@universite-paris-saclay.fr>
2025-12-22 21:02:27 +0100

    feat:IGED-174: Add StartupMissionTask class

--- LaGrandeEcoleDuDroit/News/Data/StartupAnnouncementTask.swift
+++ LaGrandeEcoleDuDroit/News/Data/StartupAnnouncementTask.swift
@@ -1,17 +1,17 @@
 class StartupAnnouncementTask {
     private let networkMonitor: NetworkMonitor
-    private let userRepository: UserRepository
     private let announcementRepository: AnnouncementRepository
+    private let resendAnnouncementUseCase: ResendAnnouncementUseCase
     private let tag = String(describing: StartupAnnouncementTask.self)
 
     init(
         networkMonitor: NetworkMonitor,
-        userRepository: UserRepository,
-        announcementRepository: AnnouncementRepository
+        announcementRepository: AnnouncementRepository,
+        resendAnnouncementUseCase: ResendAnnouncementUseCase
     ) {
         self.networkMonitor = networkMonitor
-        self.userRepository = userRepository
         self.announcementRepository = announcementRepository
+        self.resendAnnouncementUseCase = resendAnnouncementUseCase
     }
     
     func run() {
@@ -23,12 +23,10 @@ class StartupAnnouncementTask {
     
     private func sendUnsentAnnouncements() async {
         do {
-            guard let userId = self.userRepository.currentUser?.id else { return }
-            let announcements = try await announcementRepository.getLocalAnnouncements(authorId: userId)
-            
+            let announcements = try await announcementRepository.getLocalAnnouncements()
             for announcement in announcements {
                 if case .publishing = announcement.state {
-                    try await self.announcementRepository.createRemoteAnnouncement(announcement: announcement)
+                    await resendAnnouncementUseCase.execute(announcement: announcement)
                 }
             }
         } catch {
