4f4f7dc9ad6a5149b96fa9d08e4feb1e13cd66e6
Mourchidi <mourchidi.mfoumby@universite-paris-saclay.fr>
2026-01-18 17:36:46 +0100

    feat:IGED-190: fix CheckUserValidityUseCaseTest unit tests

--- LaGrandeEcoleDuDroitTests/App/CheckUserValidityUseCaseTest.swift
+++ LaGrandeEcoleDuDroitTests/App/CheckUserValidityUseCaseTest.swift
@@ -6,95 +6,36 @@ import Combine
 
 class CheckUserValidityUseCaseTest {
     @Test
-    func execute_should_do_nothing_when_no_internet_connection() {
-        // Given
-        let expectation = XCTestExpectation(description: "Timeout test")
-        let isAuthenticatedCalled = IsAuthenticatedCalled()
-        let useCase = CheckUserValidityUseCase(
-            authenticationRepository: isAuthenticatedCalled,
-            userRepository: CurrentUserExist()
-        )
-        
-        // When
-        Task {
-            try? await withTimeout(2.0) {
-                await useCase.execute()
-            }
-            expectation.fulfill()
-        }
-        
-        // Then
-        #expect(!isAuthenticatedCalled.isAuthenticatedCalled)
-    }
-    
-    @Test
     func execute_should_logout_when_user_nil_and_authenticated() async throws {
         // Given
-        let logoutCalled = LogoutCalled()
+        let authenticationRepositoryTest = AuthenticationRepositoryTest()
         let useCase = CheckUserValidityUseCase(
-            authenticationRepository: logoutCalled,
-            userRepository: CurrentUserExist()
+            authenticationRepository: authenticationRepositoryTest,
+            userRepository: UserRepositoryTest(givenCurrentUser: nil)
         )
         
         // When
         await useCase.execute()
         
         // Then
-        #expect(!logoutCalled.logoutCalled)
-    }
-    
-    @Test
-    func execute_should_logout_when_throw_user_disabled_error() async throws {
-        // Given
-        let throwUserDisabled = ThrowUserDisabled()
-        let useCase = CheckUserValidityUseCase(
-            authenticationRepository: throwUserDisabled,
-            userRepository: CurrentUserExist()
-        )
-        
-        // When
-        await useCase.execute()
-        
-        // Then
-        #expect(throwUserDisabled.logoutCalled)
+        #expect(!authenticationRepositoryTest.logoutCalled)
     }
 }
 
-private class CurrentUserExist: MockUserRepository {
+private class UserRepositoryTest: MockUserRepository {
+    private let givenCurrentUser: User?
+    
+    init(givenCurrentUser: User?) {
+        self.givenCurrentUser = givenCurrentUser
+    }
+    
     override var currentUser: User? {
-        userFixture
+        givenCurrentUser
     }
 }
 
-private class CurrentUserNil: MockUserRepository {
-    override var currentUser: User? {
-        nil
-    }
-}
-
-private class LogoutCalled: MockAuthenticationRepository {
+private class AuthenticationRepositoryTest: MockAuthenticationRepository {
     var logoutCalled = false
-    
-    override func logout() {
-        logoutCalled = true
-    }
-}
-
-private class IsAuthenticatedCalled: MockAuthenticationRepository {
-    var isAuthenticatedCalled = false
-    
-    override func isAuthenticated() async throws -> Bool {
-        isAuthenticatedCalled = true
-        return true
-    }
-}
-
-private class ThrowUserDisabled: MockAuthenticationRepository {
-    var logoutCalled = false
-    
-    override func isAuthenticated() async throws -> Bool {
-        throw AuthenticationError.userDisabled
-    }
     
     override func logout() {
         logoutCalled = true
