953d813545b0bea73e02e46f05020301f020d419
Mourchidi <mourchidi.mfoumby@universite-paris-saclay.fr>
2026-01-24 17:04:58 +0100

    feat:IGED-208: Add condition for push on release with archive event

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-if [ $CI_PULL_REQUEST_TARGET_BRANCH = "development" ] && [ $CI_XCODEBUILD_ACTION = "build" ]; then
+if [[ $CI_PULL_REQUEST_TARGET_BRANCH = "development" ]] && [[ $CI_XCODEBUILD_ACTION = "build" ]]; then
     ./debug/pre_build_debug_script.sh
 fi
 
-if [[ $CI_PULL_REQUEST_TARGET_BRANCH == release/* || $CI_BRANCH == release/* ]] && [[ $CI_XCODEBUILD_ACTION == "build" ]]; then
+if [[ $CI_PULL_REQUEST_TARGET_BRANCH == release/* ]] && [[ $CI_XCODEBUILD_ACTION == "build" ]]; then
+    ./release/pre_build_release_script.sh
+fi
+
+if [[ $CI_BRANCH == release/* ]] && [[ $CI_XCODEBUILD_ACTION == "archive" ]]; then
     ./release/pre_build_release_script.sh
 fi
